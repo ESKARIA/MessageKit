263478e7c7bddcb726283cfa2a3bb42f8a8ff40f
Emil Karimov <emvakar@gmail.com>
2020-09-11 15:32:12 +0300

    Auto stash before merge of "master" and "origin/development"

--- Sources/Layout/LinkPreviewMessageSizeCalculator.swift
+++ Sources/Layout/LinkPreviewMessageSizeCalculator.swift
@@ -22,7 +22,6 @@
  SOFTWARE.
  */
 
-import Foundation
 import UIKit
 
 open class LinkPreviewMessageSizeCalculator: TextMessageSizeCalculator {
--- Sources/Protocols/LinkItem.swift
+++ Sources/Protocols/LinkItem.swift
@@ -22,7 +22,7 @@
  SOFTWARE.
  */
 
-import Foundation
+import UIKit
 import CoreGraphics
 import UIKit
 
